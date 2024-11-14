49aa5a31150f835c256650439fe4ec103622c398
anaviico <anavicom@correo.ugr.es>
2024-11-14 21:20:09 +0100

    solucionando errores

--- test/test_routes.py
+++ test/test_routes.py
@@ -1,6 +1,5 @@
+import pytest
 
 def test_question_page(client):
-    """Test para verificar que la página de preguntas carga correctamente"""
     response = client.get('/question')
     assert response.status_code == 200
-    assert b'Pregunta' in response.data  
