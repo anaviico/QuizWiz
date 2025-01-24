9e0f671e7bf6d525ed34424c5908eeb320f04322
anaviico <anavicom@correo.ugr.es>
2025-01-24 20:27:44 +0100

    test

--- QuizWiz_Backend/test_bd.py
+++
@@ -1,8 +0,0 @@
-from extensions import db
-from models import Question
-from app import app
-
-with app.app_context():
-    questions = Question.query.all()
-    for question in questions:
-        print(f"ID: {question.id}, Category: {question.category}, Text: {question.text}")
--- test/test_app.py
+++ test/test_app.py
@@ -1,7 +1,7 @@
 from app import app
 from extensions import db
 
-def test_home_page(client):
+def test_app(client):
     response = client.get('/')
     assert response.status_code == 200
     assert b'Bienvenido a QuizWiz' in response.data
